0f3fd297ee33f12398a87aa0d6559a4a8ba5ee37
Héctor Aguado <haguadop@gmail.com>
2018-07-01 21:24:16 +0200

    Primer Commit

---
+++ app/src/main/java/com/haguado/marvel_herores_mvvm/HeroesListActivity.kt
@@ -0,0 +1,12 @@
+package com.haguado.marvel_herores_mvvm
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class HeroesListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_heroes_list)
+    }
+}
